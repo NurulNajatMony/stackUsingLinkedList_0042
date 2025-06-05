09d4ceb2b59e8fda087a55038caf37730d3fa7ba
NurulNajatMony <nurulmony505@gmail.com>
2025-06-05 11:27:25 +0700

    menambahkan constructor stack untuk inisialisasi top

--- stack.cpp
+++ stack.cpp
@@ -18,5 +18,10 @@ class Stack {
 private:
     node *top; // Pointer to the top node of the stack
 
+public:
+    Stack() {
+        top = NULL; // Initialize the stack with a null top pointer
+    }
 
+  
  
